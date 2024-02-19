92d3f6650c61a774fac44c59eb6516798c724689
LiaoYK <lucky5877112@gmail.com>
2024-02-19 23:20:50 +0800

    -Add feature report -Add feature sufficient

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -2,6 +2,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -30,20 +31,42 @@ resources = {
     "coffee": 100,
 }
 
+MachineOn = True
+TotalMoneyGotten = 0
 
-def define_the_coffee_type(sz_type):
-    coffee_type = ""
-    if sz_type == "espresso":
-        coffee_type = "espresso"
-    elif sz_type == "latte":
-        coffee_type = "latte"
-    elif sz_type == "cappuccino":
-        coffee_type = "cappuccino"
-    elif sz_type == "off":
+
+def make_the_coffee(sz_type):
+    print(f"your order is {sz_type}.")
+    sufficient = check_the_coffee_ingredient(sz_type)
+    if not sufficient[0]:
+        print(f"Sorry there is not enough {sufficient[1]}.")
+
+
+def show_ingredients_amount():
+    print(f"""
+    water: {resources["water"]}
+    milk: {resources["milk"]}
+    coffee: {resources["coffee"]}
+    money: {TotalMoneyGotten}
+    """)
+
+
+def check_the_coffee_ingredient(sz_type):
+    if resources["water"] < MENU[sz_type]["ingredients"]["water"]:
+        return [False, "water"]
+    if resources["milk"] < MENU[sz_type]["ingredients"]["milk"]:
+        return [False, "milk"]
+    if resources["coffee"] < MENU[sz_type]["ingredients"]["coffee"]:
+        return [False, "coffee"]
+    return [True, ""]
+
+
+while MachineOn:
+    UserAnswer = input("What would you like? (espresso/latte/cappuccino): ")
+    if UserAnswer == "report":
+        show_ingredients_amount()
+    elif UserAnswer == "off":
         print("Turn off the machine!!")
-        return
-    print(f"your order is {coffee_type}.")
-
-
-CoffeeType = input("Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ")
-define_the_coffee_type(CoffeeType)
+        MachineOn = False
+    elif UserAnswer == "espresso" or UserAnswer == "latte" or UserAnswer == "cappuccino":
+        make_the_coffee(UserAnswer)
